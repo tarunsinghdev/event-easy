81f5a044cbbffa74495cda965bf306ade5258f71
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-02-02 10:45:44 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function render() {
+  ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function () {
+    setTimeout(render);
+  });
+}
+
+render();
